521ab8750fee1c0511e459fc1d4fb6dfb3563787
Sonnnnnnnnnn <son829700@gmail.com>
2025-10-31 20:42:47 +0700

    coaching

--- src/components/coaching/CoachingList.jsx
+++ src/components/coaching/CoachingList.jsx
@@ -123,7 +123,7 @@ const CoachingList = ({ selectedCoachId, setSelectedCoachId }) => {
                     </p>
                 </div>
 
-                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+                <div className="grid md:grid-cols-2 lg:grid-cols-1 gap-8">
                     {coaches.map((coach) => {
                         const isSelected = selectedCoachId === coach.id;
                         return (
